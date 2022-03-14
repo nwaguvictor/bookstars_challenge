d92c74146e4bce08a48891d22845e4293a947653
nwaguvictor <leofizzy3@gmail.com>
2022-03-15 00:27:52 +0100

    fix: mongo_uri config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ dotenv.config();
 export { db } from './db';
 export const APP_NAME = process.env.APP_NAME || 'Bookstars';
 export const PORT = process.env.PORT || 4000;
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/bookstar';
 export const ENVIRONMENT = process.env.NODE_ENV || 'development';
 export const BCRYPT_SALT = Number(process.env.BCRYPT_SALT) || 10;
